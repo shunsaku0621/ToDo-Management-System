55e21fe3a67a9bc747bbf1edc350fdf44f8c8db9
shunsaku0621 <hamagi.0621@icloud.com>
2022-06-16 20:37:56 +0900

    controller名修正

--- src/main/java/com/dmm/task/controller/createController.java
+++ src/main/java/com/dmm/task/controller/createController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminPageController {
-	@GetMapping("/adminPage")
+public class createController {
+	@GetMapping("/main/create")
 	public String adminPage() {
-		return "adminPage";
+		return "create";
 	}
 }
