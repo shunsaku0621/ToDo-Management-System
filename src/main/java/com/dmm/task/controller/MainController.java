e8325f7325c84fa1aee6241e369993be3ef7face
shunsaku0621 <hamagi.0621@icloud.com>
2022-06-16 20:25:19 +0900

    認証機能の修正

--- src/main/java/com/dmm/task/controller/MainController.java
+++ src/main/java/com/dmm/task/controller/MainController.java
@@ -1,11 +1,12 @@
 package com.dmm.task.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserPageController {
-	@GetMapping("/userPage")
+public class MainController {
+	@GetMapping("/main")
 	public String userPage() {
 		return "main";
 	}
