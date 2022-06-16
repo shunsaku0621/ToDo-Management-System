d87bedaa22ac78080fa21d9c51b30555f2d9e37b
shunsaku0621 <hamagi.0621@icloud.com>
2022-06-16 20:54:18 +0900

    CSSファイルなどを認証から除外

--- src/main/java/com/dmm/task/controller/MainController.java
+++ src/main/java/com/dmm/task/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	@GetMapping("/main")
-	public String userPage() {
+	public String mainIndex() {
 		return "main";
 	}
 }
--- src/main/java/com/dmm/task/controller/createController.java
+++ src/main/java/com/dmm/task/controller/createController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CreateController {
 	@GetMapping("/main/create")
-	public String adminPage() {
+	public String createIndex() {
 		return "create";
 	}
 }
